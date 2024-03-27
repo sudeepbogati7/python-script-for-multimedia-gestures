10c5e24c240820c6b13af0caeb8b7e74c01c5598
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-03-27 19:55:45 +0545

    CV2 library setup for camera

--- hand_detection.py
+++ hand_detection.py
@@ -48,3 +48,31 @@ class handDetector():
                     cv2.circle(img, (cx, cy),5,color, cv2.FILLED)
 
         return lmList
+
+
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(1)
+    detector = handDetector(maxHands=1)
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img,z_axis=True,draw=False)
+        if len(lmList) != 0:
+            print(lmList[4])
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 255), 3)
+
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+if __name__ == "__main__":
+    main()
