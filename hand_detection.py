411525c96ec1a5bf3f7aadd8ed151682a9749694
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-03-27 19:54:11 +0545

    detect hand structure

--- hand_detection.py
+++ hand_detection.py
@@ -1,14 +1,50 @@
 import cv2
-import os
-import cv2
 import mediapipe as mp
+import time
 
-class HandDetector:
-    def __init__(self):
-        self.mp_hands = mp.solutions.hands
-        self.hands = self.mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.7, min_tracking_confidence=0.5)
 
-    def detect_hand_landmarks(self, frame):
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(rgb_frame)
-        return results
+class handDetector():
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
+                                        self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def findPosition(self, img, handNo=0, draw=True, color =  (255, 0, 255), z_axis=False):
+
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+             #   print(id, lm)
+                h, w, c = img.shape
+                if z_axis == False:
+                   cx, cy = int(lm.x * w), int(lm.y * h)
+                    # print(id, cx, cy)
+                   lmList.append([id, cx, cy])
+                elif z_axis:
+                    cx, cy, cz = int(lm.x * w), int(lm.y * h), round(lm.z,3)
+                    # print(id, cx, cy, cz)
+                    lmList.append([id, cx, cy, cz])
+
+                if draw:
+                    cv2.circle(img, (cx, cy),5,color, cv2.FILLED)
+
+        return lmList
